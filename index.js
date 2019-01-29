f460e7ae1e1bdb7b7aa6a6ac02578103767a32f4
Jeffrey Yu <jwy5140@gmail.com>
2019-01-29 06:53:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// add solution here
+function theBeatlesPlay(a, b)
+{
+  var ab = [];
+  for (var n = 0; n < a.length; n++)
+  {
+    ab[n] = a + " plays " + b;
+  }
+  
+}
+
+function johnLennonFacts(c)
+{
+  while (c)
+}
